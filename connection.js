ae24e7e595fb8fc1dac40ab8905f9229a183b8d9
dgmali <mohamed.ali@datagearbi.com>
2023-01-28 00:14:38 +0200

    change connection to VM postgresql

--- connection.js
+++ connection.js
@@ -2,14 +2,32 @@
 this file allows us to connect to our postgres database
 */
 const Pool= require('pg').Pool;
-
-const pool = new Pool({
-    host: "postgresql-107647-0.cloudclusters.net",
+/* oled connection: 
+host: "postgresql-107647-0.cloudclusters.net",
     // host:"https://travbusdb.netlify.app/",
     user: "admin",
     port: 10259,
     password: "P@ssw0rd",
     database: "TravbusDB"
+*/
+
+/* new connection 
+  host: "34.133.61.239",
+    // host:"https://travbusdb.netlify.app/",
+    user: "mali",
+    port: 5432,
+    password: "P@ssw0rd",
+    database: "travbusdb"
+
+*/
+
+const pool = new Pool({
+    host: "34.133.61.239",
+    // host:"https://travbusdb.netlify.app/",
+    user: "mali",
+    port: 5432,
+    password: "P@ssw0rd",
+    database: "travbusdb"
 });
 
 module.exports = pool
